7fa820d72d854123d690a8853e9858b4317eb5cf
Molly <mojonesycreative@gmail.com>
2022-04-22 18:40:24 -0400

    practice effect hook with cleanup

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
 import './App.css';
+import React, { useState } from 'react';
 import ProfileEdit from './ProfileEdit';
 
 function App() {
-  return <ProfileEdit />
+  const [userId, setUserId] = useState(1);
+
+  const userIds = [1, 2, 3, 4];
+
+  return (
+    <div className="App">
+      {userIds.map((id) => (
+        <button key={id} onClick={() => setUserId(id)}>
+          User ID {id}
+        </button>
+      ))}
+      <h2>User ID {userId}</h2>
+      <ProfileEdit userId={userId} />
+    </div>
+  );
 }
 
 export default App;
--- src/ProfileEdit.js
+++ src/ProfileEdit.js
@@ -1,22 +1,37 @@
-import { useState, useEffect} from "react";
+import React, { useState, useEffect} from "react";
 
-function ProfileEdit() {
+function ProfileEdit({ userId }) {
     const [user, setUser] = useState({});
-    console.log('render', user);
   
+    // Load user //
     useEffect(() => {
-    console.log(useEffect);
-      async function loadUsers() {
-        const response = await fetch(
-          "https://jsonplaceholder.typicode.com/users/1"
-        );
-        const userFromAPI = await response.json();
-        console.log('setUser', userFromAPI);
-        setUser(userFromAPI);
-      }
-      loadUsers();
-    }, []); // Passing [] so that it only runs the effect once
+      setUser({});
+      const abortController = new AbortController();
+      async function loadUser() {
+        try {
+          const response = await fetch(
+            `https://jsonplaceholder.typicode.com/users/${userId}`,
+            { signal: abortController.signal }
+          );
+          const userFromAPI = await response.json();
+          setUser(userFromAPI);
 
+        } catch (error) {
+          if (error.name === "AbortError") {
+            // Ignore `AbortError` //
+            console.log("Aborted", userId);
+          } else {
+            throw error;
+          }
+        }
+      }
+
+      loadUser();
+
+      return () => abortController.abort();
+    }, [userId]); // <-- Added dependency on `userId`
+
+    // Set document title //
     useEffect(() => {
         if (user.username) {
           document.title = `${user.username} : Edit Profile`;
@@ -25,11 +40,12 @@ function ProfileEdit() {
         }
       }, [user]); // Rerun this effect when the user changes
 
-
+    // Change handler //
     const changeHandler = event => {
         setUser({ ...user, [event.target.name]: event.target.value });
       }
 
+    // Submit handler //
     const submitHandler = async (event) => {
         event.preventDefault();
         const response = await fetch(
@@ -43,33 +59,37 @@ function ProfileEdit() {
         console.log("Saved user!", savedData);
       };
   
-
+    // Form return //
     if (user.id) {
       // `user.id` is truthy after the API call returns
       return (
         <form name="profileEdit" onSubmit={submitHandler}>
-          <div>
-            <label htmlFor="username">User Name:</label>
-            <input
-              id="username"
-              name="username"
-              type="text"
-              required={true}
-              value={user.username}
-              onChange={changeHandler}
-            />
-          </div>
-          <div>
-            <label htmlFor="email">Email:</label>
-            <input 
-                id="email" 
-                name="email" 
-                type="email" 
+          <fieldset>
+            <legend>API User ID: {user.id}</legend>
+            <div>
+              <label htmlFor="username">User Name:</label>
+              <input
+                id="username"
+                name="username"
+                type="text"
                 required={true}
-                value={user.email}
-                onChange={changeHandler} />
-          </div>
+                value={user.username}
+                onChange={changeHandler}
+              />
+            </div>
+            <div>
+              <label htmlFor="email">Email:</label>
+              <input 
+                  id="email" 
+                  name="email" 
+                  type="email" 
+                  required={true}
+                  value={user.email}
+                  onChange={changeHandler} 
+                />
+            </div>
             <button type="submit">Save</button>
+          </fieldset>
         </form>
       );
     }
